c7d789b80c81ac3a850fd2c8e5b298465a5524b4
Evan Tuazon <etuazon2@mail.ccsf.edu>
2023-01-09 22:59:31 -0800

    dice roll implemented

--- app/dice.js
+++ app/dice.js
@@ -1,30 +1,47 @@
-let dice = 
+/*
+
+var dice = 
 {
       sides: ["Regular", "Switch", "Fakie", "Nollie"],
 
-      roll: function () 
+      roll: function() 
       {
-        let randomElement = sides[Math.floor(Math.random() * (sides.length - 1))];
+        var randomElement = sides[Math.floor(Math.random() * (sides.length - 1))];
         return randomElement;
       }
 }
     
+var button = document.getElementById('button');
     
-    
-    
-function printNumber(number) 
+function rollAndPrint()
 {
-      let placeholder = document.getElementById('placeholder');
-
-      placeholder.innerHTML = number;
+      var result = dice.roll();
+      printNumber(result);
 }
     
-let button = document.getElementById('button');
-    
-button.onclick = function() 
+function printText(text) 
 {
-      let result = dice.roll();
-      printNumber(result);
-};
-    
-    
+      var placeholder = document.getElementById('placeholder');
+
+      placeholder.textContent = text;
+}
+
+*/
+
+var stances = ["Regular", "Switch", "Fakie", "Nollie"]
+
+var spins = ["Backside", "Frontside", "", "", "", ""]
+
+let rotations = ["", "180", "360", "180 shove", "360 shove", ""]
+
+let flips = ["Kickflip", "Heelflip", "", "", "", ""]
+
+
+document.getElementById("myButton").onclick = function()
+{
+      document.getElementById("stance").innerHTML = stances[Math.floor(Math.random() * stances.length)]
+      document.getElementById("spin").innerHTML = spins[Math.floor(Math.random() * spins.length)]
+      document.getElementById("rotation").innerHTML = rotations[Math.floor(Math.random() * rotations.length)]
+      document.getElementById("flip").innerHTML = flips[Math.floor(Math.random() * flips.length)]
+      
+}
